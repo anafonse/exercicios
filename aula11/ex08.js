32d8c21435ed72217974985896bdb64cdd77da7e
Ana <118758557+anafonse@users.noreply.github.com>
2023-08-16 10:50:42 -0300

    Add files via upload

---
+++ aula11/ex08.js
@@ -0,0 +1,15 @@
+ var velocidade = 120
+ console.log(`A velocidade do seu carro é ${velocidade}KM/h`)
+
+ if (velocidade > 60) { //condição simples
+     console.log(`Voce ultrapassou a velocidade permitida! MULTADO!!`)
+ } 
+
+
+//  var pais = 'China'
+
+//  if(pais =='Brasil') {
+//      console.log('Brasileiro')
+//  } else {
+//      console.log('Estrangeiro')
+//  } 
